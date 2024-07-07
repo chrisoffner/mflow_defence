c2ba387d7ea23444398c52eb6f098d3ebb64fc73
Chris Offner <chrisoffner@pm.me>
2024-07-07 20:07:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Adversarial Defences with Manifold-Learning Flows
+
+This repository contains code by Claire Bräuer, Pablo Robles Cervantes, Yufei Liu, and Chris Offner for a project that was done as part of the **Computational Intelligence Lab 2024** at ETH Zurich.
+
+We investigate the effectiveness of manifold-learning normalizing flows (**[M-Flows](https://arxiv.org/abs/2003.13913)**) as an adversarial defence method for existing classifiers. Our project aims to assess the extent to which projecting input samples onto the learned data manifold and shifting them towards regions of higher data likelihood can improve adversarial robustness. By utilising the manifold-learning capabilities of M-Flows, we explore a novel ap- proach to mitigating the impact of adversarial attacks on deep neural networks.
---
+++ scripts/pngs_to_mp4.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import cv2
+import os
+from tqdm import tqdm
+import re
+
+# Function to extract the numerical part from the filename
+def extract_number(filename):
+    s = re.findall(r'\d+', filename)
+    return int(s[0]) if s else -1
+
+def create_video_from_images(image_dir, output_filename, fps=24):
+    # Get all image file names
+    image_files = [img for img in os.listdir(image_dir) if img.endswith(".png")]
+
+    # Sort the files based on the numerical part extracted from each file name
+    image_files = sorted(image_files, key=extract_number)
+    
+    if not image_files:
+        print("No images found in the directory.")
+        return
+    
+    # Determine the width and height from the first image
+    sample_img = cv2.imread(os.path.join(image_dir, image_files[0]))
+    height, width, _ = sample_img.shape
+    
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')  # Change codec here
+    out = cv2.VideoWriter(str(output_filename), fourcc, fps, (width, height))
+
+    # Check if VideoWriter was successfully opened
+    if not out.isOpened():
+        print("Error: Could not open VideoWriter. Check codec and file path.")
+        return
+    
+    for image_file in tqdm(image_files):
+        img = cv2.imread(os.path.join(image_dir, image_file))
+        out.write(img)  # Write frame to video
+    
+    # Release everything if job is finished
+    out.release()
+    print(f"Video written to {output_filename}")
+
+
+if __name__ == "__main__":
+    # Define the path to the directory containing the images
+    FIGURES_DIR = Path("/Users/chrisoffner3d/Documents/Dev/ETH/CIL Project Code/manifold-flow/figures/spiral_boundaries_noise_20")
+    OUTPUT_PATH = FIGURES_DIR / "spiral_boundaries_noise_20.mp4"
+
+    create_video_from_images(FIGURES_DIR, OUTPUT_PATH, fps=30)
